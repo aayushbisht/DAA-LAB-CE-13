cb92c1bf8f2f2e96c94287590dfc17f0c088c808
aayushbisht <97892535+aayushbisht@users.noreply.github.com>
2022-04-19 18:10:32 +0530

    Add files via upload

---
+++ WEEK-4/Q-2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#define MAX 1000
+using namespace std;
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+int comp=0,swaps=0;
+void quick_sort(int arr[],int lb,int ub)
+{
+    int pivot=arr[lb];
+    int i=lb,j=ub;
+
+    if (lb>=ub)
+       return ;
+    else{
+        while (i<j)
+        {
+            comp++;
+            while (pivot>=arr[i] && i<j)
+                  i++;
+            while (pivot<arr[j])
+                  j--;
+            if (i<j)
+             swap(&arr[i],&arr[j]);
+        }
+        swaps++;
+        swap(&arr[lb],&arr[j]);
+        quick_sort(arr,lb,j-1);
+        quick_sort(arr,j+1,ub);
+    }
+
+}
+
+
+int main()
+{
+   int arr[MAX];
+    int n;
+    cout << "Enter size" << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+        
+        quick_sort(arr,0,n-1);
+
+        for (int i=0;i<n;i++)
+           cout<<arr[i];
+
+        cout<<endl<<"Swaps:"<<swaps<<endl;
+        cout<<"Comparisions:"<<comp<<endl;
+    
+     
+
+
+    return 0;
+}
